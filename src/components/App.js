ac3097f40a04dad6ebb76054c0e88a919030b887
genzaraky <amadou140589@gmail.com>
2021-04-08 06:24:12 +0100

    app component converted to statefull component. Implemented handleClick function. Pass total to Display as result. Pass handleClick to ButtonPannel

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import Calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const newState = Calculate(this.state, buttonName);
+    this.setState(() => ({ ...newState }));
+  }
+
+  render() {
+    const { total } = this.state;
+    return (
+      <>
+        <Display result={total} />
+        <ButtonPanel handleClick={this.handleClick} />
+      </>
+    );
+  }
 }
 
 export default App;
